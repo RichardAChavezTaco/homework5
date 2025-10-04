638b7dc37ac6fad321011bed45d48aa785855772
Richard Chavez <thecovermantaco63@gmail.com>
2025-10-04 04:07:06 -0500

    CardDetails Done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 
-const fullname = faker.person.fullName()
-const firstname = faker.person.firstName() || 'john'
-const lastname = faker.person.lastName() || 'Doe'
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({firstName: firstname, lastName: lastname})
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.username({firstName: firstname, lastName: lastname}).toLowerCase()
@@ -12,12 +12,14 @@ const username = faker.internet.username({firstName: firstname, lastName: lastna
 
 <template>
   <!-- {{ fullname }} <br /> -->
-  <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
-    <img class="object-fill h-480 w-960 rounded-t-xl" v-bind:src="faker.image.urlLoremFlickr({category: 'dogs', height: 480, width: 960})" />
-    <div class="p-4">
-      <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-      <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-      <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+  <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+    <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
+      <img class="object-fill h-480 w-960 rounded-t-xl" v-bind:src="faker.image.urlLoremFlickr({category: 'dogs', height: 480, width: 960})" />
+      <div class="p-4">
+        <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+      </div>
     </div>
-  </div>
+  </RouterLink>
 </template>
